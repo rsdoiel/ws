6e15ae36e57780dde6e76e5c4b90c6dbdc598556
R. S. Doiel <rsdoiel@gmail.com>
2014-07-10 22:54:43 -0700

    snapshot, still broken

--- ottoengine/ottoengine.go
+++ ottoengine/ottoengine.go
@@ -66,53 +66,48 @@ func Load(root string) ([]Program, error) {
 }
 
 func createRequestLiteral(r *http.Request) string {
-	var (
-		headers string
-		src     string
-	)
+	var src string
 
-	buf, err := json.Marshal(r.Header)
+	buf, err := json.Marshal(r)
 	if err != nil {
-		headers = "[]"
+		src = "{}"
 	} else {
-		headers = string(buf)
+		src = string(buf)
 	}
 
-	//FIXME: need to handle GET, POST, PUT, DELETE methods
-	src = fmt.Sprintf(`{
-            Headers: %s,
-            Method: %q,
-            URL: %q,
-            Proto: %q,
-            Referrer: %q,
-            UserAgent: %q
-    }`, headers, r.Method, r.URL, r.Proto, r.Referer(), r.UserAgent())
 	return src
 }
 
 func createResponseLiteral() string {
 	src := `{
-        code: 200,
-        status: "OK",
-        headers: {},
-        header_keys: [],
-        getHeader: function (key) {
-            if (this.headers[key.toLowerCase()] !== undefined) {
-                return this.headers[key.toLowerCase()];
-            }
-            return false;
-        },
-        setHeader: function (key, value) {
-            this.headers[key.toLowerCase()] = value;
-            return (this.headers[key.toLowerCase()] === value);
-        },
-        collectHeaderKeys: function () {
-            return this.header_keys = Object.keys(this.headers);
-	    },
-	    popHeaderKey: function () {
-	        return this.header_keys.pop();
-	    }
-    }`
+		code: 200,
+		status: "OK",
+		headers: {"content-type": "text/plain"},
+		content: null,
+		getHeader: function (key) {
+			if (this.headers[key.toLowerCase()] !== undefined) {
+				return this.headers[key.toLowerCase()];
+			}
+			return false;
+		},
+		setHeader: function (key, value) {
+			this.headers[key.toLowerCase()] = value;
+			return (this.headers[key.toLowerCase()] === value);
+		},
+		collectHeaderKeys: function () {
+			return this.header_keys = Object.keys(this.headers);
+		},
+		popHeaderKey: function () {
+			return this.header_keys.pop();
+		},
+		setContent: function (content) {
+			this.content = content;
+			return (this.content === content);
+		},
+		getContent: function () {
+			return this.content;
+		}
+	}`
 	return src
 }
 
@@ -132,12 +127,21 @@ func IsHTML(value otto.Value) bool {
 	return strings.HasPrefix(blob, "<!DOCTYPE html>")
 }
 
+type Response struct {
+	code int
+	status string
+	headers map[string]string
+	content []bytes
+}
+
+
 func Engine(program Program) {
 	http.HandleFunc(program.Route, func(w http.ResponseWriter, r *http.Request) {
 		var (
 			vm               *otto.Otto
 			script           string
-			closing_script   string
+                        closure_script   string
+                        run_script       string
 			request_literal  string
 			response_literal string
 		)
@@ -148,48 +152,57 @@ func Engine(program Program) {
 		// 2. Create a fresh Response object literal.
 		response_literal = createResponseLiteral()
 
-		// 3. Setup the VM for the Route with closure
-		//vm = program.VM
+		// 3. Setup the VM for the Route with our closure
+		vm = program.VM
 		script = string(program.Source)
-		closing_script = fmt.Sprintf(`(function () { Request = %s; Response = %s; return %s;}())`, request_literal, response_literal, script)
-		fmt.Printf("DEBUG closing_script: %s\n", closing_script)
+		closure_script = `JSON.stringify((function(Request,Response){var value = %s;if (value) { Response.setContent(value); };return Response;}(%s,%s)));`
+		run_script = fmt.Sprintf(closure_script, script, request_literal, response_literal)
 
 		// 4. Run the VM wrapped with a closure containing`Request, Response
-		output, err := vm.Run(closing_script)
+		output, err := vm.Run(run_script)
 		if err != nil {
 			msg := fmt.Sprintf("Script: %s", err)
+			wslog.LogResponse(500, "Internal Server Error",
+					   r.Method, r.URL, r.RemoteAddr, program.Filename, msg)
+			http.Error(w, "Internal Server Error", 500)
+			return
+		}
+fmt.Println(output)// DEBUG
+		json_src, err := output.ToString()
+		if err != nil {
+			msg := fmt.Sprintf("Conversion to JSON: %s", err)
 			wslog.LogResponse(500, "Internal Server Error",
 				r.Method, r.URL, r.RemoteAddr, program.Filename, msg)
 			http.Error(w, "Internal Server Error", 500)
 			return
 		}
+		
 
-		// 5. update headers from responseObject
-		/*
-			content_type := ""
-			vm.Run(`Response.collectHeaderKeys()`)
-			key_cnt_value, _ := vm.Run("Response.header_keys.length")
-			key_cnt, _ := key_cnt_value.ToInteger()
-			for i := int64(0); i < key_cnt; i++ {
-				key_value, _ := vm.Run(`Response.popHeaderKey();`)
-				key, _ := key_value.ToString()
-				if key != "" {
-					value_value, _ := vm.Run(fmt.Sprintf("Response.getHeader(%q);", key))
-					value, _ := value_value.ToString()
-					if value != "" {
-						if key == "content-type" {
-							content_type = value
-						}
-						w.Header().Set(key, value)
-					}
-				}
-			}
-			// 6. Calc fallback content types if needed.
-			if content_type == "" && IsJSON(output) {
-				w.Header().Set("Content-Type", "application/json")
-			} else if content_type == "" && IsHTML(output) {
-				w.Header().Set("Content-Type", "text/html")
-			}
+			   // 5. update headers from responseObject
+			   /*
+				   content_type := ""
+				   vm.Run(`Response.collectHeaderKeys()`)
+				   key_cnt, _ := key_cnt_value.ToInteger()
+				   for i := int64(0); i < key_cnt; i++ {
+					   key_value, _ := vm.Run(`Response.popHeaderKey();`)
+					   key, _ := key_value.ToString()
+					   if key != "" {
+						   value_value, _ := vm.Run(fmt.Sprintf("Response.getHeader(%q);", key))
+						   value, _ := value_value.ToString()
+						   if value != "" {
+							   if key == "content-type" {
+								   content_type = value
+							   }
+							   w.Header().Set(key, value)
+						   }
+					   }
+				   }
+				   // 6. Calc fallback content types if needed.
+				   if content_type == "" && IsJSON(output) {
+					   w.Header().Set("Content-Type", "application/json")
+				   } else if content_type == "" && IsHTML(output) {
+					   w.Header().Set("Content-Type", "text/html")
+				   }
 		*/
 		// 5. send the output to the browser.
 
