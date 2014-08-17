185097a4dbe54cecbc1b6e225f15f01c4c788cec
R. S. Doiel <rsdoiel@yahoo.com>
2014-08-17 10:45:49 -0700

    added verboseness to test-ws.sh

--- test-ws.sh
+++ test-ws.sh
@@ -9,14 +9,16 @@ else
     exit 1
 fi
 if [ -f "./ws" ]; then
+    echo "Stale ws found. Removing."
     /bin/rm ./ws
 fi
 # build ws
-echo "Build ws.go"
+echo "Building ws.go"
 go build ws.go
 # Try to run ws in this directory.
 echo "Try to run ws"
 if [ -f "./ws" ]; then
+    echo "Build OK, starting with otto and TLS false"
     ./ws -otto=false -tls=false
 else
     echo "Something went wrong building ws."
