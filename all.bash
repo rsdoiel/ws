6337f2bd10717f8878cffdbf3701eaa0f201a215
R. S. Doiel <rsdoiel@gmail.com>
2015-03-22 13:30:39 -0700

    Started re-organizing code and files

--- all.bash
+++ all.bash
@@ -2,7 +2,7 @@
 #
 GO=$(which go)
 if [ "$GO" = "" ]; then
-    echo 'Must install Golang first"
+    echo "Must install Golang first"
     echo "See http://golang.org for instructions"
     exit 1
 fi
