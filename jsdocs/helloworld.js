659e8f8d8a95bd07537566a1b6c8b1c854ece433
R. S. Doiel <rsdoiel@gmail.com>
2016-02-01 08:17:50 -0800

    tweaked demo site

--- jsdocs/helloworld.js
+++ jsdocs/helloworld.js
@@ -4,7 +4,7 @@
 (function (req, res) {
     console.log("Incoming request", JSON.stringify(req));
     res.code = 200;
-    res.headers = [{"Content-Type": "text/plain"}];
-    res.content = "Hello World!";
+    res.headers = [{"Content-Type": "text/html"}];
+    res.content = "<DOCTYPE html>\n<html><head><title>Testing Server Side JavaScript</title></head><body><h1>Testing Server Side JavaScript</h1><p>Hello World!</p><a href=\"/\">homepage</a></p></body></html>";
     console.log("Processed response", JSON.stringify(res));
 }(Request, Response));
